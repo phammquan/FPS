f82716dead417288a419ab53cf5027e7d2b6211e
phammquan <phamminhq91@gmail.com>
2024-12-14 13:36:40 +0700

    switch_weapon

---
+++ Assets/00 SCRIPT/SwitchWeapons.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchWeapons : MonoBehaviour
+{
+    private int selectedWeapon = 0;
+    public Animation _Animation;
+    public AnimationClip _switch;
+    
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if(selectedWeapon >= transform.childCount - 1)
+                selectedWeapon = 0;
+            else
+                selectedWeapon++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if(selectedWeapon <= 0)
+                selectedWeapon = transform.childCount - 1;
+            else
+                selectedWeapon--;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectedWeapon = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            selectedWeapon = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            selectedWeapon = 4;
+        }
+        
+        if(previousSelectedWeapon != selectedWeapon)
+            SelectWeapon();
+        
+    }
+    
+    void SelectWeapon()
+    {
+        
+        if(selectedWeapon >= transform.childCount)
+            selectedWeapon = 0;
+        _Animation.Stop();
+        _Animation.Play(_switch.name);
+        int i = 0;
+        foreach (Transform _weapon in transform)
+        {
+            if (i == selectedWeapon)
+            {
+                _weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                _weapon.gameObject.SetActive(false);
+            }
+            i++;
+        }
+    }
+}
